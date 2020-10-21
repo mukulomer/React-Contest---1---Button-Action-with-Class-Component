4647f0dce62b031daff8a64497c589b1fc7fe1a2
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-21 21:23:35 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      para: <p></p>
+      para: false
     };
   }
 
   handelClick = () => {
-    let newPara = (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
+    let newPara = true;
     this.setState({
       para: newPara
     });
   };
 
+  rederPara() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
+
   render() {
     return (
       <div id="main">
         <button id="click" onClick={this.handelClick}>
-          Click
+          click
         </button>
-        {this.state.para}
+        {this.state.para ? this.rederPara() : ""}
       </div>
     );
   }
